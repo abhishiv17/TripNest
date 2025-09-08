b668c2cb2663be7613cbc2baa933f8e018c2d234
Abhishek M S <abhishekshivanagoudar@gmail.com>
2025-09-08 23:38:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -96,6 +96,10 @@ app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews",reviewRouter);
 app.use("/",userRouter);
 
+app.get("/", (req, res) => {
+    res.render("index");   
+});
+
 app.all(/.*/, (req, res, next) => {
            next(new ExpressError(404, "Page Not Found"));
       });
